5b4731ca49ec6fd5f4410de32656fde038a1062d
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-09-22 13:57:55 +0530

    my first commit of this project

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.css';
+
+const NavBar = () => {
+  return (
+    <div className='nav-cnt'>
+      <div className='nav-heading-cnt'>
+        <p className='nav-heading'>Notes App</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/mainpage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../components/navbar/NavBar';
+import './mainpage.css';
+
+const MainPage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default MainPage;
