1c4209524381fd14840042feaef686ee22add9d0
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-09-22 19:08:22 +0530

    body is created

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,10 +3,29 @@ import './body.css';
 
 const Body = () => {
   return (
-    <div>
+    <div className='body-cnt'>
       <div className='search-cnt'>
-        <input type='search' class='search-bar' placeholder='search' />
+        <input type='search' className='search-bar' placeholder='search' />
       </div>
+      <div className='btn-cnt'>
+        <button className='add-btn'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='16'
+            height='16'
+            fill='currentColor'
+            className=' bi-plus-lg'
+            viewBox='0 0 16 16'
+          >
+            <path
+              fillRule='evenodd'
+              d='M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z'
+            />
+          </svg>
+        </button>
+      </div>
+      <div className='notes-cnt'></div>
+      <div className='modal-cnt'></div>
     </div>
   );
 };
